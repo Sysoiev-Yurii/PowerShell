c472ae280a4fefcb3f7ee18ec115d63818e75765
alerickson <25858831+alerickson@users.noreply.github.com>
2024-08-09 16:12:07 -0700

    Add PSResoureGetACRTests to validation set

--- tools/ci.psm1
+++ tools/ci.psm1
@@ -225,7 +225,7 @@ function Invoke-CITest
 {
     [CmdletBinding()]
     param(
-        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests')]
+        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'PSResoureGetACRTests')]
         [string] $Purpose,
         [ValidateSet('CI', 'Others')]
         [string] $TagSet,
@@ -695,7 +695,7 @@ function Invoke-LinuxTestsCore
 {
     [CmdletBinding()]
     param(
-        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'All')]
+        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'PSResoureGetACRTests', 'All')]
         [string] $Purpose = 'All',
         [string[]] $ExcludeTag = @('Slow', 'Feature', 'Scenario'),
         [string] $TagSet = 'CI',
