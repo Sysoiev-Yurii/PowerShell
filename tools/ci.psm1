eedc24575a25856282087b1caaafe566766b2088
alerickson <25858831+alerickson@users.noreply.github.com>
2024-08-12 12:21:09 -0700

    Update spelling

--- tools/ci.psm1
+++ tools/ci.psm1
@@ -225,7 +225,7 @@ function Invoke-CITest
 {
     [CmdletBinding()]
     param(
-        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'PSResoureGetACRTests')]
+        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'PSResourceGetACRTests')]
         [string] $Purpose,
         [ValidateSet('CI', 'Others')]
         [string] $TagSet,
@@ -695,7 +695,7 @@ function Invoke-LinuxTestsCore
 {
     [CmdletBinding()]
     param(
-        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'PSResoureGetACRTests', 'All')]
+        [ValidateSet('UnelevatedPesterTests', 'ElevatedPesterTests', 'PSResourceGetACRTests', 'All')]
         [string] $Purpose = 'All',
         [string[]] $ExcludeTag = @('Slow', 'Feature', 'Scenario'),
         [string] $TagSet = 'CI',
